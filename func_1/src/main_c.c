45b34004fe4c534fb6134872c74cd1af8bdc1bea
Andrew Yaroshevych <ayaroshlvua@gmail.com>
2022-11-10 18:25:49 +0200

    Minor visual cleanup

--- func_1/src/main_c.c
+++ func_1/src/main_c.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void func (int32_t* array, size_t size);
+void func(int32_t *array, size_t size);
 
 int main() {
     size_t size = 15;
--- func_2/src/main_c.c
+++ func_2/src/main_c.c
@@ -3,14 +3,14 @@
 
 void func(float *a, float *b, float *x, size_t size);
 
-int main(){
+int main() {
     size_t size = 15;
-    float *a_coef = malloc(size * sizeof(float ));
-    float *b_coef = malloc(size * sizeof(float ));
-    float *result = malloc(size * sizeof(float ));
+    float *a_coef = malloc(size * sizeof(float));
+    float *b_coef = malloc(size * sizeof(float));
+    float *result = malloc(size * sizeof(float));
 
-    for (int i= 0; i< size; ++i) {
-        a_coef[i] = (float)rand()/(float)(RAND_MAX/1000);
+    for (int i = 0; i < size; ++i) {
+        a_coef[i] = (float) rand() / (float) (RAND_MAX / 1000);
         b_coef[i] = i + 1;
     }
 
@@ -23,4 +23,5 @@ int main(){
     free(a_coef);
     free(b_coef);
     free(result);
+    return 0;
 }
